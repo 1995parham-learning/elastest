306fadd10ec5e5d0b18201eabb9bf893f8f70ec1
Parham Alvani <parham.alvani@gmail.com>
2022-10-31 00:26:41 +0330

    fix: correct lint issues

--- actions/app.go
+++ actions/app.go
@@ -7,17 +7,17 @@ import (
 	"gopkg.in/go-playground/validator.v9"
 )
 
-// App creates new instance of Echo and configures it
+// App creates new instance of Echo and configures it.
 func App() *echo.Echo {
 	app := echo.New()
 	app.Use(middleware.Logger())
 
 	app.Use(apmechov4.Middleware())
 
-	// Validator
+	// Register validator
 	app.Validator = &DefaultValidator{validator.New()}
 
-	// Routes
+	// Register routes
 	app.GET("/hello", HelloHandler)
 
 	return app
