7d5dd6dc9f423455fb0afbce11b842dd703e9884
Parham Alvani <parham.alvani@gmail.com>
2022-10-31 00:26:44 +0330

    fix: correct lint issues

--- main.go
+++ main.go
@@ -11,10 +11,13 @@ import (
 	"github.com/1995parham-learning/elastest/actions"
 )
 
+const ShutdownTimeout = 5 * time.Second
+
 func main() {
 	log.Println("18.20 at Sep 07 2016 7:20 IR721")
 
 	app := actions.App()
+
 	go func() {
 		if err := app.Start(":8080"); err != http.ErrServerClosed {
 			log.Fatalf("API Service failed with %s", err)
@@ -27,8 +30,9 @@ func main() {
 
 	log.Println("18.20 As always ... left me alone")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), ShutdownTimeout)
 	defer cancel()
+
 	if err := app.Shutdown(ctx); err != nil {
 		log.Printf("API Service failed on exit: %s", err)
 	}
